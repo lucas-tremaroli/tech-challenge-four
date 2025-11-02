79ccd61bbf0b274fd759b0a6fe37ce122c044136
lucas-tremaroli <lucasluigitremaroli@outlook.com>
2025-11-02 20:54:14 +0100

    feat(container): docker compose
    Signed-off-by: lucas-tremaroli <lucasluigitremaroli@outlook.com>

--- src/main.py
+++ src/main.py
@@ -1,13 +1,8 @@
 import logging
 
-# Configure logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler('app.log'),
-        logging.StreamHandler()
-    ]
 )
 
 logger = logging.getLogger(__name__)
