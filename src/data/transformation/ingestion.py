db4401cd231cbcc6336cdb8b5d44253b7efabdb4
lucas-tremaroli <lucasluigitremaroli@outlook.com>
2025-11-10 00:19:34 +0100

    chore(data): store history data for training
    Signed-off-by: lucas-tremaroli <lucasluigitremaroli@outlook.com>

--- src/data/transformation/ingestion.py
+++ src/data/transformation/ingestion.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 
 
-def fetch_stock_data(ticker: str, period: str = "1y", interval: str = "1d"):
+def fetch_stock_data(ticker: str, period: str = "1w", interval: str = "1d"):
     stock = yf.Ticker(ticker)
     hist = stock.history(period=period, interval=interval)
     return hist
